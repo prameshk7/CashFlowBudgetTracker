dc260482a21e3811a91b223c4cd8888672e3b5f0
prameshlogic <pramesh.logicseed@gmail.com>
2024-12-16 08:17:43 +0545

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PrameshBudgetTracker
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
